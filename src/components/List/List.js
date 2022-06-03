2f431e528e4149bb039d0b566a6c1c8efe926544
Łukasz Staniszewski <lukas13.88@gmail.com>
2022-06-03 18:55:50 +0200

    Add selectors and action creators

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,10 +3,11 @@ import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
 import shortid from 'shortid';
+import { getAllColumns } from '../../redux/store';
 
 const List = () => {
 
-    const columns = useSelector(state => state.columns);
+    const columns = useSelector(getAllColumns);
 
     return (
         <div className={styles.list}>
---
+++ src/utils/strContains.js
@@ -0,0 +1,7 @@
+export const strContains = (str1, str2) => {
+    if(str1.toLowerCase().includes(str2.toLowerCase())) {
+        return true;
+    } else {
+        return false;
+    }
+}
