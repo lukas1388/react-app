add1066ba8321b34e49be1624700f1473ff89d06
Łukasz Staniszewski <lukas13.88@gmail.com>
2022-06-06 18:09:20 +0200

    Add new component Home, add to App components About, Favorite

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import Hero from './components/Hero/Hero';
-import SearchForm from './components/SearchForm/SearchForm';
-import List from './components/List/List';
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
-import About from './components/About/About';
+import Home from './components/Home/Home';
 import Favorite from './components/Favorite/Favorite';
+import About from './components/About/About';
 
-const App = () => {
+function App() {
   return (
     <main>
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Home />
+        <About />
+        <Favorite />
       </Container>
     </main>
   );
-};
+}
 
 export default App;
