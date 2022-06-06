061138b5a9748fd07b20ab1bb21504a6969cfe87
Łukasz Staniszewski <lukas13.88@gmail.com>
2022-06-06 17:41:21 +0200

    Add component NavBar - exercise 1

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import styles from './NavBar.module.scss';
+import Container from '../Container/Container';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <Container>
+                <div className={styles.nav}>
+                    <div className={styles.icon}>
+                        <a href="#"><span className={'fa fa-tasks'} /></a>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="favorite">Favorite</a></li>
+                            <li><a href="about">About</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </Container>
+        </nav>
+    );
+};
+
+export default NavBar;
